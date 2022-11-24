ebd643e3fae6aaf67efac572df95ad15951c2296
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2022-11-24 11:46:40 +0100

    Add solution to exercise 3.0

---
+++ 3.0.js
@@ -0,0 +1,30 @@
+let learners = [
+  "Arno Volts",
+  "Aurélien Mariaule",
+  "Aurore Lemaire",
+  "Benjamin Porsont",
+  "Céline Verreydt",
+  "Corentin Miserque",
+  "Dominique Coppée",
+  "Edouard de Romrée de Vichenet",
+  "Hugo Goorickx ",
+  "Jofrey Houyoux",
+  "Jonathan Manes",
+  "jonathan bajoux",
+  "Laura Wilhelmi",
+  "Lysie Soyez",
+  "Marnie Benalia",
+  "Mathilde     Cornelis",
+  "Milo Bonnet",
+  "Nadim El Nakadi",
+  "Nathalie Vanden Abeele",
+  "Nathalie Goffette",
+  "Nour Everaert",
+  "Pierre Mauriello",
+  "Quentin Bource ",
+  "Virginie Dourson",
+];
+
+for (let learner of learners) {
+    console.log(learner);
+}

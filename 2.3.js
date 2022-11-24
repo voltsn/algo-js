12edf434d3eadc449577d8b95ed31253d9019aad
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2022-11-24 10:40:05 +0100

    Add solution to exercise 2.3

---
+++ 2.3.js
@@ -0,0 +1,21 @@
+// Display even numbers between 1 and 100
+// using a while and a for loop.
+let lowerBound = 1;
+let upperBound = 100;
+
+// while loop edition.
+let counter = lowerBound;
+while (counter <= upperBound){
+    if (counter % 2 === 0){
+        console.log(counter);
+    }
+
+    counter++;
+}
+
+// for loop edition.
+for (let i = lowerBound; i <= upperBound; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}

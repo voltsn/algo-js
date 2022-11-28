093ce9d08f0e5d64de191961ab7eb1a75c926ebd
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2022-11-28 12:45:46 +0100

    Remove test function calls

--- manhattan.js
+++ manhattan.js
@@ -3,7 +3,3 @@ const manhattan = (streetA, avenueA, streetB, avenueB) => {
     let yDistance = Math.abs(avenueA - avenueB);
     return xDistance + yDistance;
 }
-
-console.log(manhattan(1, 1, 2, 2)); // 2
-console.log(manhattan(1, 1, 1, 1)); // 0
-console.log(manhattan(5, 4, 3, 2)); // 4
